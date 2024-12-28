aecfbb5fa7bed79ad9e4878ebf323bb7628f2cc1
MN <m.nakae.0103@gmail.com>
2024-12-28 14:18:13 +0900

    DBと踏み台サーバーの構築

---
+++ Stackable-Infrastructure/src/Stackable.Infrastructure/Ec2Stack.cs
@@ -0,0 +1,43 @@
+﻿using Amazon.CDK.AWS.EC2;
+
+namespace Stackable.Infrastructure;
+internal class Ec2Stack : Construct
+{
+    public SecurityGroup BastionSecurityGroup { get; private set; }
+
+    public Ec2Stack(Construct scope, string id, StackableSystemProvider systemProvider, Vpc vpc) : base(scope, id)
+    {
+        // 秘密鍵の保存名
+        var keyPair = new KeyPair(this, systemProvider.GetId("ec2-kp"), new KeyPairProps
+        {
+            KeyPairName = systemProvider.GetId("ec2-kp"),
+        });
+
+        BastionSecurityGroup = new SecurityGroup(this, systemProvider.GetId("bastion-sg"), new SecurityGroupProps
+        {
+            Vpc = vpc,
+            AllowAllOutbound = true,
+        });
+
+        // 指定したIPアドレスからのみ踏み台サーバーとへのSSHアクセスを許可
+        BastionSecurityGroup.AddIngressRule(Peer.AnyIpv4(), Port.Tcp(22), "Allow SSH access from my IP");
+
+        _ = new Instance_(this, systemProvider.GetId("bastion"), new InstanceProps
+        {
+            InstanceName = systemProvider.GetId("bastion"),
+            Vpc = vpc,
+            VpcSubnets = new SubnetSelection
+            {
+                SubnetType = SubnetType.PUBLIC,
+            },
+            SecurityGroup = BastionSecurityGroup,
+            InstanceType = InstanceType.Of(InstanceClass.T2, InstanceSize.MICRO),
+            MachineImage = new AmazonLinuxImage(new AmazonLinuxImageProps
+            {
+                Generation = AmazonLinuxGeneration.AMAZON_LINUX_2023,
+                CpuType = AmazonLinuxCpuType.X86_64
+            }),
+            KeyPair = keyPair,
+        });
+    }
+}
---
+++ Stackable-Infrastructure/src/Stackable.Infrastructure/RdsStack.cs
@@ -0,0 +1,49 @@
+﻿using Amazon.CDK.AWS.EC2;
+using Amazon.CDK.AWS.RDS;
+
+namespace Stackable.Infrastructure;
+internal class RdsStack : Construct
+{
+    public DatabaseInstance RdsInstance { get; private set; }
+
+    public RdsStack(
+        Construct scope,
+        string id,
+        StackableSystemProvider systemProvider,
+        Vpc vpc,
+        ISecurityGroup bastionSecurityGroup) : base(scope, id)
+    {
+        var rdsSecurityGroup = new SecurityGroup(this, systemProvider.GetId("rds-sg"), new SecurityGroupProps
+        {
+            Vpc = vpc,
+            AllowAllOutbound = true,
+        });
+
+        // 踏み台サーバーからRDSへのアクセスを許可
+        rdsSecurityGroup.AddIngressRule(bastionSecurityGroup, Port.Tcp(3306), "Allow MySQL access from Bastion host");
+
+        RdsInstance = new DatabaseInstance(this, systemProvider.GetId("db"), new DatabaseInstanceProps
+        {
+            Engine = DatabaseInstanceEngine.Mysql(new MySqlInstanceEngineProps
+            {
+                Version = MysqlEngineVersion.VER_8_4_3
+            }),
+            InstanceType = Amazon.CDK.AWS.EC2.InstanceType.Of(InstanceClass.BURSTABLE4_GRAVITON, InstanceSize.MICRO),
+            Vpc = vpc,
+            VpcSubnets = new SubnetSelection { SubnetType = SubnetType.PRIVATE_ISOLATED },
+            SecurityGroups = [rdsSecurityGroup],
+            MultiAz = false,
+            AllocatedStorage = 20,
+            StorageType = StorageType.GP2,
+            DeletionProtection = false,
+            DatabaseName = systemProvider.GetId("db"),
+            // DB接続情報のSecretsManagerの保存先
+            Credentials = Credentials.FromGeneratedSecret("admin", new CredentialsBaseOptions
+            {
+                SecretName = $"{systemProvider.ProjectName}-db-secret",
+            }),
+            // スタック削除時にRDSインスタンスを削除する
+            RemovalPolicy = RemovalPolicy.DESTROY,
+        });
+    }
+}
